dfedf665b58576aa5751449a7abfad527c4685f5
GunnerStraiker1 <victorcito001@hotmail.com>
2018-12-30 23:12:14 -0600

    Send Mesages Pt1

--- sendcontact.php
+++ sendcontact.php
@@ -1,38 +1,32 @@
 <?php
 
-// Define some constants
-define( "RECIPIENT_NAME", "John Doe" ); //UPDATE THIS TO YOUR NAME
-define( "RECIPIENT_EMAIL", "john@example.com" ); //UPDATE THIS TO YOUR EMAIL ID
-define( "EMAIL_SUBJECT", "Website Visitor Message" ); //UPDATE THIS TO YOUR SUBJECT
+// specify your email here
 
-// Read the form values
-$success = false;
-$senderName = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
-$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$original_message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
-$message = 'Name: '.$senderName.'<br/>Email: '.$senderEmail.'<br/>Message: '.$original_message;
+$to = 'victorox100@gmail.com';
 
-// If all values exist, send the email
-if ( $senderName && $senderEmail && $message ) {
-  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $senderName . " <" . $senderEmail . ">\n";
-  $headers .= "MIME-Version: 1.0\n"; 
-  $headers .= "Content-Type: text/HTML; charset=ISO-8859-1\n";
-  $success = mail( $recipient, EMAIL_SUBJECT, $message, $headers );
-}
+// Assigning data from $_POST array to variables
+if (isset($_POST['name'])) {$name = $_POST['name'];}
+if (isset($_POST['email'])) {$from = $_POST['email'];}
+// if (isset($_POST['telefono'])) {$company = $_POST['telefono'];}
+if (isset($_POST['message'])) {$message = $_POST['message'];}
 
-if ( $success )
-{
-?>
-	<script>
-		window.location='thanks.html';
-	</script>
-<?php
-}
-else
-{
-	echo "<h1>There was a problem sending your message. Please try again.</h1>";
+// Construct subject of the email
+$subject = 'Cotizacion de ' . $name;
+
+// Construct email body
+$body_message = 'Name: ' . $name . "\r\n" . 'Email: ' . $from . "\r\n" . 'Message: ' . $message . "\r\n";
+
+// Construct headers of the message
+$headers = 'From: ' . $from . "\r\n";
+
+$mail_sent = mail($to, $subject, $body_message, $headers);
+
+if ($mail_sent == true) {
+    http_response_code(200);
+    echo "Thank You! Your message has been sent.";
+    
+} else {
+    http_response_code(500);
+    echo "Oops! Something went wrong and we couldn't send your message.";
 }
 ?>
-
-
